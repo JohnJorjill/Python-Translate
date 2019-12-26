71bacac7203108e8364e80bd2ddc50dc17fa6f98
Bat-Ulzii Orgil <48145722+JohnJorjill@users.noreply.github.com>
2019-12-26 15:48:30 +0800

    Update pytranslate.py

--- pytranslate.py
+++ pytranslate.py
@@ -1,11 +1,11 @@
-from translate import Translator
+from translate import Translator 
 
-translator = Translator(to_lang="zh")
+translator = Translator(to_lang="zh") # Translator class-s translator object uusgej bna
 try:
-	with open('./test.txt', mode='r') as my_file:
+	with open('./test.txt', mode='r') as my_file: # ./test.txt file-g ongoilgood text variable dotor buh contentiin huulaad translate hiigeed test-zh.txt file dotor hadaglah
 		text = my_file.read()
 		translation = translator.translate(text)
 		with open('./test-zh.txt', mode='w') as my_file2:
 			my_file2.write(translation)
-except FileNotFoundError as e:
+except FileNotFoundError as e: # FileNotFoundError aldaa zaaval doorh msg print hiih
 	print('please check your path')
